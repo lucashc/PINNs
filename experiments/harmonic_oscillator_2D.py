3573ca1e897f3d33efc98253c89582676aafa6f8
Lucas Crijns <lucascrijns@gmail.com>
2021-05-26 13:43:11 +0200

    2D harmonic oscillators

---
+++ experiments/harmonic_oscillator_2D.py
@@ -0,0 +1,55 @@
+import torch
+import numpy as np
+from scipy.constants import hbar, m_e
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from models.helper import dfx, Sin
+from models.model import EigenvalueProblemModel
+
+# Solution on bounds
+x_min = -6
+x_max = 6
+
+def PDE_loss(x, psi, E):
+    psi_dx = dfx(x, psi)
+    psi_ddx = dfx(x, psi_dx[:,0])[:,0] + dfx(x, psi_dx[:,1])[:,1]
+    diff = 1/2 * psi_ddx + (E-potential(x)) * psi # -hbar**2/(2*m_e) * psi_ddx + potential(x) * psi  - E * psi
+    loss = torch.mean(diff**2)
+    return loss
+
+def potential(x):
+    k = 4
+    return k*(x[:, 0]**2 + x[:, 1]**2)/2
+
+def compose_psi(x, N):
+    f_b = 0
+    dt = x - x_min
+    psi = f_b + (1 - torch.exp(-dt[:, 0])) * (1 - torch.exp(-dt[:, 1])) * (1 - torch.exp(dt[:,0]-x_max*2)) * (1 - torch.exp(dt[:,1]-x_max*2)) * N.reshape(-1)
+    return psi
+
+def perturb(grid, x_max=0, x_min=1, n_train=101, sig=0.05):
+    noise = torch.randn_like(grid) * sig
+    x = grid + noise
+    # Make sure perturbation still lay in domain
+    x[x[:,0] < x_min, 0] = x_min - x[x[:,0] < x_min, 0]
+    x[x[:,0] > x_max, 0] = 2*x_max - x[x[:,0] > x_max, 0]
+    x[x[:,1] < x_min, 1] = x_min - x[x[:,1] < x_min, 1]
+    x[x[:,1] > x_max, 1] = 2*x_max - x[x[:,1] > x_max, 1]
+    # Make sure at least one point is at the boundaries
+    # x[:n_train,0] = torch.zeros_like(x[:n_train,1])
+    # x[-n_train:,0] = torch.ones_like(x[:n_train,1])
+    # x[0::n_train,1] = torch.zeros_like(x[::n_train,1])
+    # x[n_train-1::n_train,1] = torch.ones_like(x[::n_train,1])
+
+    return x
+
+def driver(index):
+    return -2+0.25*(index//2000)
+
+grid1D = torch.linspace(x_min, x_max, 20)
+grid2D_x, grid2D_y = torch.meshgrid(grid1D,grid1D)
+grid = torch.cat([grid2D_x.reshape(-1,1), grid2D_y.reshape(-1,1)], dim=1)
+
+model = EigenvalueProblemModel([2, 50, 50, 1], Sin, compose_psi, PDE_loss, lr=8e-3, start_eigenvalue=1.0)
+model.train(driver, 3000, grid, perturb, int(1e5), max_required_loss=1e-2, rtol=0.01, fraction=6)
+model.plot_history()
